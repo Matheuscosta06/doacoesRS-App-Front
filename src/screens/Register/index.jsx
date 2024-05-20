1e07407541f2cd6b219189b8d7a550b8e9096928
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-20 15:43:02 -0300

    feat: Add Register screen and styles
    The code changes add a new Register screen component and its corresponding styles. This screen is responsible for rendering a registration form.

---
+++ src/screens/Register/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import styles from './styles'
+
+export default function Register() {
+    return (
+        <View style={styles.container}>
+            <Text>Register Screen</Text>
+        </View>
+    )
+}
