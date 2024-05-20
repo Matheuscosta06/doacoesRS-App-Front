012b77e5d947b41d811d3ba9a13b36b0a8f24816
joaosantos564 <joaosantos@aluno.senai.br>
2024-05-20 15:42:54 -0300

    init carousel

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
+import Carousel from 'react-native-snap-carousel'
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Title title={'Home today is beutiful day'} />
+      <Carousel
+      //propriedades
+       //arquivos para add no carrossel:
+      data={}
+      //Componente da imagem (formato):
+      renderItem={}
+      //o tamanho da tela:
+      sliderWidth={}
+      //o tamanho do item
+      itemHeight={}
+      useScrollView={true}
+       />
     </View>
   )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -4,8 +4,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
   }
 });
 
