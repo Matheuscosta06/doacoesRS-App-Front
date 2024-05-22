63b71726b5cdb2b431c228d2b2212856434ef4d2
matheuscosta <matheuszambon564@gmail.com>
2024-05-22 14:39:49 -0300

    feat: Add Gift and Sobre screens to private tab routes
    The code changes include adding the Gift and Sobre screens to the private tab routes. This allows authenticated users to access the Gift screen to view gift details and the Sobre screen to learn more about the app.

--- src/routes/PrivateTab.routes.jsx
+++ src/routes/PrivateTab.routes.jsx
@@ -3,7 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { Feather } from "@expo/vector-icons";
 import Home from "../screens/Home";
+import Gift from "../screens/Gift";
 import Category from "../screens/Category";
+import Sobre from "../screens/Sobre";
 import { Text } from 'react-native';
 
 
@@ -52,6 +54,39 @@ const PrivateTab = () => {
           }
           }
         />
+        <Tab.Screen name="Gift" component={Gift}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="gift"
+                size={24}
+                color={focused ? "#4B6584" : "#b9c3cd"} />
+            ),
+            tabBarLabel: ({ focused, color }) => (
+              <Text style={{ color, fontSize: 13 }}>Presente</Text>
+            ),
+            tabBarActiveTintColor: "#4B6584",
+            tabBarInactiveTintColor: "#b9c3cd",
+          }
+          }
+        />
+        <Tab.Screen name="Sobre" component={Sobre}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="users"
+                size={24}
+                color={focused ? "#4B6584" : "#b9c3cd"} />
+            ),
+            tabBarLabel: ({ focused, color }) => (
+              <Text style={{ color, fontSize: 13 }}>Sobre</Text>
+            ),
+            tabBarActiveTintColor: "#4B6584",
+            tabBarInactiveTintColor: "#b9c3cd",
+          }
+          }
+        />
+
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/screens/Sobre/index.jsx
+++ src/screens/Sobre/index.jsx
@@ -2,10 +2,10 @@ import { View, Text } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
 
-export default function Profile() {
+export default function Sobre() {
   return (
     <View style={styles.container}>
-      <Title title={'Profile today is beutiful day'} />
+      <Title title={'Sobre today is beutiful day'} />
     </View>
   )
 }
