c4856b9f4c2d47b62fb96b31652d1c08f0b86c06
joaosantos564 <joaosantos@aluno.senai.br>
2024-05-22 11:56:56 -0300

    init header

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,32 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
+  const navigation = useNavigation();
+
+
   return (
     <ScrollView style={styles.container}>
-      <Title title={'Home today is beutiful day'} />
+      <View style={styles.head}>
+      <Image
+          source={require("../../../assets/MapRS.png")}
+          style={styles.imgHeader}
+        />
+        <View style={styles.titleHeader}>
+        <Text style={styles.title0}>Ajude o</Text>
+        <Text style={styles.title01}>Rio Grande do Sul</Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Doe Agora</Text>
+        </TouchableOpacity>
+
+      </View>
     </ScrollView>
   )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,9 +1,43 @@
 import { StyleSheet } from "react-native";
+import { Button } from "react-native-web";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#d9d9d9"
+  },
+  head: {
+    backgroundColor: '#283444',
+    display: 'flex',
+    height: 450,
+    borderBottomStartRadius: 50,
+    borderBottomEndRadius: 50
+  },
+  imgHeader: {
+    width:350,
+    height: 330,
+    marginTop: -55,
+    marginLeft: 230
+  },
+  titleHeader:{
+    marginLeft: 20
+  },
+  title0: {
+    color: '#fff',
+    fontSize: 50,
+    marginTop: -30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title01:{
+    color: '#FFA41B',
+    fontSize: 40
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: "#FFA41B",
+    alignItems: "center",
+    borderRadius: 10,
   }
 });
 
