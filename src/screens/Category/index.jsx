ba5393f1bbfb243b9d7c91ec1fc44c922089a245
matheuscosta <matheuszambon564@gmail.com>
2024-05-22 17:19:16 -0300

    feat: Improve Category screen with interactive donation options
    The code changes enhance the Category screen by adding interactive donation options for PET's, Alimentos/Água, Roupas, Higiene, and Dinheiro. This provides users with a more engaging and user-friendly experience when selecting donation categories.

--- src/screens/Category/index.jsx
+++ src/screens/Category/index.jsx
@@ -1,22 +1,58 @@
-import { View, Text, ImageBackground } from 'react-native'
-import styles from './styles'
-import Title from '../../components/Title'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './styles';
+import React from 'react';
 
 export default function Category() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('../../../assets/MapRS.png')} style={styles.maps}>
+      <ScrollView>
         <Text style={styles.titulo}>O que doar?</Text>
 
+        <TouchableOpacity style={styles.card}>
 
+          <View style={styles.cardzin}>
 
+            <Text style={styles.cardText}>PET's</Text>
+          </View>
 
+        </TouchableOpacity>
 
+        <TouchableOpacity style={styles.card}>
 
+          <View style={styles.cardzin}>
 
+            <Text style={styles.cardText}>Alimentos/Água</Text>
+          </View>
 
+        </TouchableOpacity>
 
-      </ImageBackground>
+        <TouchableOpacity style={styles.card}>
+
+          <View style={styles.cardzin}>
+
+            <Text style={styles.cardText}>Roupas</Text>
+          </View>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+
+          <View style={styles.cardzin}>
+
+            <Text style={styles.cardText}>Higiene</Text>
+          </View>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+
+          <View style={styles.cardzin}>
+            <Image source={require('../../../assets/pix.jpeg')} style={styles.piximg} />
+            <Text style={styles.cardText}>Dinheiro</Text>
+          </View>
+
+        </TouchableOpacity>
+      </ScrollView>
     </View>
-  )
+  );
 }
--- src/screens/Category/styles.js
+++ src/screens/Category/styles.js
@@ -6,24 +6,51 @@ const styles = StyleSheet.create({
     backgroundColor: "#242e46",
     alignItems: "center",
     justifyContent: "center",
-    fontFamily: "josefin-sans-regular"
   },
   titulo: {
     fontSize: 36,
     color: "#fff",
-    fontWeight: "bold", 
+    fontWeight: "bold",
     textAlign: "center",
     marginTop: 50,
+    marginBottom: 30,
+    fontFamily: 'JosefinSans_700Bold',
+  },
+  card: {
+    marginVertical: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignSelf: 'center',
+    width: "100%",
 
   },
 
-  maps: {
-    flex: 1,
-    resizeMode: "cover",
-    width: "60",
-    height: "90",
-    opacity: 0.5,
-    marginTop: 15,
+  cardzin: {
+    backgroundColor: '#FFA41B',
+    justifyContent: "space-between",
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 50,
+    display: 'flex',
+    flexDirection: 'row',
+
+
+  },
+  cardText: {
+    color: '#000',
+    fontSize: 24,
+    marginRight: 10,
+    fontFamily: 'JosefinSans_700Bold',
+  },
+  piximg: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    marginRight: 0,
+
+
+
+
   },
 
 });
