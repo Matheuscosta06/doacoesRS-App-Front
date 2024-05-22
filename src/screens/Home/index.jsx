83f11e7bba1788605998bd95d632abd64918ac61
joaosantos564 <joaosantos@aluno.senai.br>
2024-05-22 09:25:28 -0300

    install fonts

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,27 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
-import Carousel from 'react-native-snap-carousel'
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Title title={'Home today is beutiful day'} />
-      <Carousel
-      //propriedades
-       //arquivos para add no carrossel:
-      data={}
-      //Componente da imagem (formato):
-      renderItem={}
-      //o tamanho da tela:
-      sliderWidth={}
-      //o tamanho do item
-      itemHeight={}
-      useScrollView={true}
-       />
-    </View>
+    </ScrollView>
   )
 }
-
 
 
