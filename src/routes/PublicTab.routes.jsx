f6ae17057447cf94d9f4307dadfe8628cc1abf8f
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-22 07:50:20 -0300

    ```text feat: Add Register screen and Login screen to public tab routes
    The code changes include adding a new Register screen component and its corresponding styles to the public tab routes. This allows users to register for an account in addition to logging in.

--- src/routes/PublicTab.routes.jsx
+++ src/routes/PublicTab.routes.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
 import Login from "../screens/Login";
+import Register from "../screens/Register";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,7 +14,15 @@ const PublicTab = () => {
         headerShown: true,
       }}>
         <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="Register" component={Register} />
+
+
       </Tab.Navigator>
+
+      
+
+
+
       <StatusBar style="auto" />
     </NavigationContainer>
   );
