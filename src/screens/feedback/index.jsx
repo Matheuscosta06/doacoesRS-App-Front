4f5ce493bcae67797511891545e8f1b87327ffbd
matheuscosta <matheuszambon564@gmail.com>
2024-05-27 11:50:53 -0300

    feat: Add FAQ section to Feedback screen

--- src/screens/feedback/index.jsx
+++ src/screens/feedback/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity, ScrollView, } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import styles from './styles';
 import React from 'react';
 
@@ -7,7 +7,32 @@ export default function Feedback() {
     <View style={styles.container}>
       <ScrollView>
         <Text style={styles.txt1}>Pricipais</Text>
+        <hr style={styles.linha1}></hr>
         <Text style={styles.txt2}>Dúvidas</Text>
+        <hr style={styles.linha2}></hr>
+
+
+        <View style={styles.card1}>
+          <Text style={styles.txt2}>Por qual motivo vocês quiseram criar esse site?</Text>
+          <Text style={styles.txt3}>Pois tinhamos a vontade de ajudar o Rio grande do Sul com o que sabemos fazer, que são aplicativos</Text>
+        </View>
+        <View style={styles.card2}>
+          <Text style={styles.txt2}>Quais são os planos para o futuro?</Text>
+          <Text style={styles.txt3}>Temos a intenção de expandir para outros estados e até mesmo para outros países</Text>
+        </View>
+        <View style={styles.card3}>
+          <Text style={styles.txt2}>Como posso ajudar?</Text>
+          <Text style={styles.txt3}>Você pode ajudar compartilhando o site com seus amigos e familiares</Text>
+        </View>
+
+        <View style={styles.feed}>
+          <Text style={styles.txt2}>Deixe sua dúvida ou Feedback</Text>
+          <TextInput placeholderTextColor={"#fff"} placeholder='Digite Aqui' style={styles.input} />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.txtbtn}>Enviar</Text>
+        </TouchableOpacity>
+
       </ScrollView>
     </View>
   );
--- src/screens/feedback/styles.js
+++ src/screens/feedback/styles.js
@@ -7,6 +7,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  txt1: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "bold",
+    fontFamily: "josefinSans_700Bold",
+    marginTop: 20,
+
+  },
+  txt2: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "bold",
+    fontFamily: "josefinSans_700Bold",
+    marginLeft: 80,
+    marginTop: 20,
+  },
+  linha1: {
+    width: 200,
+    height: 4,
+    backgroundColor: "#ffA41B",
+    marginRight: 80,
+
+  },
+  linha2: {
+    width: 200,
+    height: 4,
+    backgroundColor: "#ffA41B",
+    marginLeft: 80,
+  },
+
 });
 
 export default styles;
