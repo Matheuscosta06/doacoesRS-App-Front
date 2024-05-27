592b21806a9f46af15f991c3825203d04a5a3da8
matheuscosta <matheuszambon564@gmail.com>
2024-05-27 11:20:42 -0300

    feat: Create Feedback screen

---
+++ src/screens/feedback/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#242e46",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
