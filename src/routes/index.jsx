d6be104c6cab23732ad329bc7966915f9c63bf66
matheuscosta <matheuszambon564@gmail.com>
2024-05-27 10:49:10 -0300

    chore: Remove commented code and simplify Routes component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,8 +4,9 @@ import PublicTab from "./PublicTab.routes";
 import { AuthContext } from "../contexts/AuthContext";
 
 function Routes() {
-  const { user } = useContext(AuthContext);
-  return user ? <PrivateTab /> : <PublicTab />
+  // const { user } = useContext(AuthContext);
+  // return user ? <PrivateTab /> : <PublicTab />
+  return <PrivateTab />
 }
 
 export default Routes;
