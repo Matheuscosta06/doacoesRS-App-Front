30b9c7bca48a32ce769559636c74622a199b6f7c
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-05-27 10:54:46 -0300

    feat: "finalização do card principal e início do card de objetivo"

--- src/screens/Sobre/index.jsx
+++ src/screens/Sobre/index.jsx
@@ -1,20 +1,26 @@
 import { View, Text, Image } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function Sobre() {
   return (
     <View style={styles.container}>
+      <ScrollView>
       <View style={styles.cardPrincipal}>
       <Image
         style={styles.fotoRS}
         source={require('../../../assets/mapa.png')}
       />
-      <Text style={styles.sobre}>Sobre a</Text>
-      <Text>NovaGenix</Text>
-      <Text>Solutions</Text>
+      <Text style={styles.sobreA}>Sobre a</Text>
+      <Text style={styles.novaG}>NovaGenix</Text>
+      <Text style={styles.solut}>Solutions</Text>
       </View>
-
+      <View style={styles.cardObj}>
+       <Text style={styles.tituloObj}>Um pouco sobre nosso objetivo com esse projeto:</Text>
+       <Text style={styles.textObj}>Desde o início, a equipe de desenvolvedores sabia que criar um site para organizar doações no Rio Grande do Sul seria especial. Com especialistas em várias áreas, a missão era criar uma plataforma intuitiva e eficiente. O designer criou uma interface amigável, enquanto os desenvolvedores trabalharam no código e na infraestrutura. O gerente de projetos coordenava as tarefas, e o responsável pela comunicação lia feedbacks e conectava-se com ONGs e doadores. Superaram desafios técnicos e emocionais. No lançamento, ver as doações e pedidos de ajuda concretizados foi gratificante. O site continua a crescer, ajudando muitos gaúchos e mostrando o poder da tecnologia e da colaboração.</Text>
+</View>
+</ScrollView>
     </View>
   )
 }
--- src/screens/Sobre/styles.js
+++ src/screens/Sobre/styles.js
@@ -4,24 +4,65 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFA41B",
-   
+
   },
-  cardPrincipal : {
+  cardPrincipal: {
     backgroundColor: "#283444",
     width: "auto",
     height: 450,
     borderBottomLeftRadius: 40,
     borderBottomRightRadius: 40,
   },
-  sobre: {
-   fontSize: 65,
-   color: "#fff"
-  },
-  fotoRS : {
+  sobreA: {
+    fontSize: 55,
+    fontFamily: "JosefinSans_700Bold",
+    color: "#fff",
+    marginLeft: 32,
 
-  marginTop: -120,
-marginLeft: 200,
+  },
+  novaG: {
+    marginTop: 15,
+    fontSize: 35,
+    marginLeft: 47,
+    fontFamily: "JosefinSans_700Bold",
+    color: "#FFA41B"
+  },
+  solut: {
+    fontSize: 35,
+    marginLeft: 163,
+    fontFamily: "JosefinSans_700Bold",
+    color: "#FFA41B"
+  },
+  fotoRS: {
+    marginTop: -120,
+    marginLeft: 200,
+  },
+  cardObj: {
+    backgroundColor: "#fff",
+    margin: 20,
+    marginTop: 50,
+    borderRadius: 20,
+    height: 1250,
+  },
+  tituloObj: {
+    fontSize: 51,
+    fontFamily: "JosefinSans_700Bold",
+    color: "#000",
+    marginTop: 50,
+
+    textAlign: "center",
+    margin: 15,
+  },
+  textObj: {
+    fontSize: 28,
+    padding: 0,
+    margin: 20,
+    textAlign: "center",
+    fontFamily: "JosefinSans_500Medium",
+    color: "#000",
   }
+
+
 });
 
 export default styles;
