12f8ec17fa76a8166f256351c085d0c744f78834
matheuscosta <matheuszambon564@gmail.com>
2024-05-27 11:20:29 -0300

    feat: Add Feedback screen to PrivateTab.routes

--- src/routes/PrivateTab.routes.jsx
+++ src/routes/PrivateTab.routes.jsx
@@ -6,6 +6,7 @@ import Home from "../screens/Home";
 import Gift from "../screens/Gift";
 import Category from "../screens/Category";
 import Sobre from "../screens/Sobre";
+import Feedback from "../screens/feedback";
 import { Text } from 'react-native';
 import DetalhesPets from "../screens/DetalhesPets";
 import { FontAwesome5 } from '@expo/vector-icons';
@@ -94,6 +95,21 @@ const PrivateTab = () => {
           }
           }
         />
+
+        <Tab.Screen name="Feedback" component={Feedback}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="message-square"
+                size={24}
+                color={focused ? "#4B6584" : "#b9c3cd"} />
+            ),
+            tabBarActiveTintColor: "#4B6584",
+            tabBarInactiveTintColor: "#b9c3cd",
+          }
+          }
+        />
+
 
       </Tab.Navigator>
     </NavigationContainer>
