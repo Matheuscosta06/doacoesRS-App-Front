1ec231107a3b4c156d8827bf0ec227655a230808
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-05-27 08:39:36 -0300

    feat: "realização da parte do card principal"

--- src/screens/Sobre/index.jsx
+++ src/screens/Sobre/index.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import styles from './styles'
 import Title from '../../components/Title'
 
 export default function Sobre() {
   return (
     <View style={styles.container}>
-      <Title title={'Sobre today is beutiful day'} />
+      <View style={styles.cardPrincipal}>
+      <Image
+        style={styles.fotoRS}
+        source={require('../../../assets/mapa.png')}
+      />
+      <Text style={styles.sobre}>Sobre a</Text>
+      <Text>NovaGenix</Text>
+      <Text>Solutions</Text>
+      </View>
+
     </View>
   )
 }
--- src/screens/Sobre/styles.js
+++ src/screens/Sobre/styles.js
@@ -3,9 +3,24 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#FFA41B",
+   
+  },
+  cardPrincipal : {
+    backgroundColor: "#283444",
+    width: "auto",
+    height: 450,
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+  },
+  sobre: {
+   fontSize: 65,
+   color: "#fff"
+  },
+  fotoRS : {
+
+  marginTop: -120,
+marginLeft: 200,
   }
 });
 
