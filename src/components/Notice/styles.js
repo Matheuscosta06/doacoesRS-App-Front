e3d21dfeaea072bdfbb87982c6b6940ec23b41ef
João Victor <joaosantos.senai564@gmail.com>
2024-05-27 11:44:32 -0300

    componente noticia

---
+++ src/components/Notice/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+   height: 1000
+  },
+  imgNotice: {
+    height: 400
+  },
+  noticia:{
+    width: 400,
+    backgroundColor: '#8c8c8c',
+    borderWidth: 6,
+    borderColor: '#000'
+  }
+  
+});
+
+export default styles;
