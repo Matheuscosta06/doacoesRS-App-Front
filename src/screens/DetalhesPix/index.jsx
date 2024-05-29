5eb57b5c30ff35bb76a3c03534897d739f9dae70
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-05-29 09:26:38 -0300

    feat: Add DetalhesPix screen with gradient background

---
+++ src/screens/DetalhesPix/index.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import styles from './styles';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
+
+export default function DetalhesPix() {
+  
+    const navigation = useNavigation();
+    
+    return (
+        <ScrollView>
+        <LinearGradient
+            colors={['', '']}
+                style={styles.container}
+            >
+        
+        </LinearGradient>
+
+        </ScrollView>
+    );
+}
