ed88bdd911a170e4e0a43c486e40072a8e62f63d
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-29 17:16:46 -0300

    chore: Update DetalhesPix screen styles and content

--- src/screens/DetalhesPix/index.jsx
+++ src/screens/DetalhesPix/index.jsx
@@ -8,25 +8,25 @@ export default function DetalhesPix() {
     const navigation = useNavigation();
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <Text style={styles.title}>Detalhes do Pix</Text>
-                <View style={styles.box}>
-                    <Text style={styles.text}>O Rio Grande do Sul está passando por momentos difíceis e muitas famílias precisam de nossa ajuda. A solidariedade e a união são essenciais para superar desafios. Com um simples gesto, você pode fazer a diferença na vida de muitas pessoas.
+        <ScrollView style={styles.container0}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Detalhes do Pix</Text>
+            <View style={styles.Chaveeqr}>
 
-                        Convidamos você a fazer um PIX e contribuir para a recuperação e apoio das famílias gaúchas. Sua doação, não importa o valor, será um alívio para aqueles que enfrentam dificuldades.</Text>
-                </View>
-                <View style={styles.Chaveeqr}>
+                <Image
+                    style={styles.qr}
+                    source={require('../../../assets/qr.png')}
+                />
+                <Text style={styles.text1}>Chave PIX: 123.456.789-10</Text>
 
-                    <Image
-                        style={styles.qr}
-                        source={require('../../../assets/qr.png')}
-                    />
-                    <Text style={styles.text}>Chave PIX: 123.456.789-10</Text>
-
-                    </View>
-               
             </View>
+            <View style={styles.box}>
+                <Text style={styles.text}>O Rio Grande do Sul está passando por momentos difíceis e muitas famílias precisam de nossa ajuda. A solidariedade e a união são essenciais para superar desafios. Com um simples gesto, você pode fazer a diferença na vida de muitas pessoas.
+
+                    Convidamos você a fazer um PIX e contribuir para a recuperação e apoio das famílias gaúchas. Sua doação, não importa o valor, será um alívio para aqueles que enfrentam dificuldades.</Text>
+            </View>
+
+        </View>
         </ScrollView>
     );
 }
--- src/screens/DetalhesPix/styles.js
+++ src/screens/DetalhesPix/styles.js
@@ -1,35 +1,63 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
+    container0: {
         flex: 1,
-        padding: 20,
-        backgroundColor: '#242e46',
+        backgroundColor: '#d9d9d9',
+    },
+    container: {
+        width: '85%',
+        height: '950',
+        backgroundColor: '#08bfb1',
+        alignItems: 'center',
+        borderRadius: 20,
+        justifyContent: 'center',
+        margin: "auto",
+        marginTop: 40,
 
     },
     title: {
-        fontSize: 24,
+        marginTop: 20,
+        fontSize: 36,
         fontWeight: 'bold',
         marginBottom: 20,
+        color: '#fff',
     },
     box: {
         padding: 20,
         marginBottom: 20,
         borderWidth: 1,
-        borderColor: '#ddd',
+        borderColor: '#d9d9d9',
         borderRadius: 5,
+        width: '80%'
     },
     text: {
         fontSize: 16,
         lineHeight: 24,
+        textAlign: 'justify',
+        fontFamily: 'josefin-sans',
+        fontWeight: 'bold',
+    },
+    text1: {
+        fontSize: 22,
+        lineHeight: 24,
+        textAlign: 'justify',
+        fontFamily: 'josefin-sans',
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 20,
     },
     Chaveeqr: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         alignItems: 'center',
+        marginLeft: 20,
+        marginTop: 20,
     },
     qr: {
-        width: 100,
-        height: 100,
+        width: 250,
+        height: 250,
         marginRight: 20,
+        borderWidth: 4,
+        borderColor: '#08bfb1',
     },
 });
