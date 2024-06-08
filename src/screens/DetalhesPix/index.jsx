34254ee6a2b1610ad1e8c6c7510465639b15f3bd
Matheuszambondacosta <matheus-costa@aluno.senai.br>
2024-06-08 18:23:46 -0300

    refactor: Update DetalhesPix screen layout and visual appearance
    Improve layout and visual appearance of the DetalhesPix screen by updating its styles. This includes adding a background image, adjusting the text alignment, and updating the font styles. This enhancement enhances the user experience by providing a visually appealing and cohesive design for the DetalhesPix screen.

--- src/screens/DetalhesPix/index.jsx
+++ src/screens/DetalhesPix/index.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react';
-import styles from './styles';
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
 
 export default function DetalhesPix() {
 
     const navigation = useNavigation();
 
     return (
-        <ScrollView style={styles.container0}>
-        <View style={styles.container}>
-            <Text style={styles.title}>Detalhes do Pix</Text>
-            <View style={styles.Chaveeqr}>
-
-                <Image
-                    style={styles.qr}
-                    source={require('../../../assets/qr.png')}
-                />
-                <Text style={styles.text1}>Chave PIX: 123.456.789-10</Text>
-
-            </View>
-            <View style={styles.box}>
-                <Text style={styles.text}>O Rio Grande do Sul está passando por momentos difíceis e muitas famílias precisam de nossa ajuda. A solidariedade e a união são essenciais para superar desafios. Com um simples gesto, você pode fazer a diferença na vida de muitas pessoas.
-
-                    Convidamos você a fazer um PIX e contribuir para a recuperação e apoio das famílias gaúchas. Sua doação, não importa o valor, será um alívio para aqueles que enfrentam dificuldades.</Text>
-            </View>
-
-        </View>
+        <ScrollView style={styles.mainContainer}>
+            <ImageBackground 
+                source={require('../../../assets/imagepx.jpg')} 
+                style={styles.background}
+            >
+                <View style={styles.contentContainer}>
+                    <Text style={styles.title}>Detalhes do Pix</Text>
+                    <View style={styles.Chaveeqr}>
+                        <Image
+                            style={styles.qr}
+                            source={require('../../../assets/qr.png')}
+                        />
+                        <Text style={styles.text1}>Chave PIX: 123.456.789-10</Text>
+                    </View>
+                    <View style={styles.box}>
+                        <Text style={styles.text}>
+                            O Rio Grande do Sul está passando por momentos difíceis e muitas famílias precisam de nossa ajuda. 
+                            A solidariedade e a união são essenciais para superar desafios. Com um simples gesto, você pode fazer 
+                            a diferença na vida de muitas pessoas.
+                            {'\n\n'}
+                            Convidamos você a fazer um PIX e contribuir para a recuperação e apoio das famílias gaúchas. Sua doação, 
+                            não importa o valor, será um alívio para aqueles que enfrentam dificuldades.
+                        </Text>
+                    </View>
+                </View>
+            </ImageBackground>
         </ScrollView>
     );
 }
--- src/screens/DetalhesPix/styles.js
+++ src/screens/DetalhesPix/styles.js
@@ -1,22 +1,32 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container0: {
+    
+    mainContainer: {
         flex: 1,
         backgroundColor: '#d9d9d9',
     },
-    container: {
+    background:{
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+    },
+
+    
+    contentContainer: {
         width: '85%',
-        height: '950',
-        backgroundColor: '#08bfb1',
+        height: 950,
         alignItems: 'center',
         borderRadius: 20,
         justifyContent: 'center',
-        margin: "auto",
+        alignSelf: 'center',
         marginTop: 20,
         marginBottom: 140,
-
     },
+    
     title: {
         marginTop: 20,
         fontSize: 36,
@@ -24,41 +34,50 @@ export default StyleSheet.create({
         marginBottom: 20,
         color: '#fff',
     },
+    
     box: {
         padding: 20,
         marginBottom: 20,
         borderWidth: 1,
         borderColor: '#d9d9d9',
         borderRadius: 5,
-        width: '80%'
+        width: '80%',
     },
+    
     text: {
         fontSize: 16,
         lineHeight: 24,
         textAlign: 'justify',
-        fontFamily: 'josefin-sans',
+        fontFamily: 'JosefinSans-Bold',
         fontWeight: 'bold',
+        color:'#fff',
+
     },
+
     text1: {
         fontSize: 22,
         lineHeight: 24,
         textAlign: 'justify',
-        fontFamily: 'josefin-sans',
+        fontFamily: 'JosefinSans-Bold', 
         fontWeight: 'bold',
         marginTop: 20,
         marginBottom: 20,
+        color:'#fff',
     },
-    Chaveeqr: {
+    qrContainer: {
         flexDirection: 'column',
         alignItems: 'center',
         marginLeft: 20,
         marginTop: 20,
     },
+
     qr: {
         width: 250,
         height: 250,
-        marginRight: 20,
         borderWidth: 4,
-        borderColor: '#08bfb1',
+        borderColor: '#000',
+        alignItems:'center',
+        borderRadius: 8,
+
     },
 });
