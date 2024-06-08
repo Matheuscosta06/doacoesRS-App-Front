051f8592704a4f981c440e90b40c80c085f6f074
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-06-08 14:38:55 -0300

    refactor: Update DetalhesAlimentos screen styles
    Improve layout and visual appearance of the DetalhesAlimentos screen by updating its styles. This includes adding a circular image, adjusting the quantity buttons, and updating the add to cart button style. These changes enhance the user experience and make the screen more visually appealing.

--- src/screens/Category/index.jsx
+++ src/screens/Category/index.jsx
@@ -11,68 +11,70 @@ export default function Category() {
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('../../../assets/wlppRS.png')} style={styles.imgBg}>
-        <View style={styles.divMain}>
-          <Text style={styles.titulo}>O que doar?</Text>
+      <ScrollView>
+        <ImageBackground source={require('../../../assets/wlppRS.png')} style={styles.imgBg}>
+          <View style={styles.divMain}>
+            <Text style={styles.titulo}>O que doar?</Text>
 
-          <View style={styles.icones1}>
-            <TouchableOpacity style={styles.card} onPress={() => navigation.navigate(`DetalhesPets`)}>
+            <View style={styles.icones1}>
+              <TouchableOpacity style={styles.card} onPress={() => navigation.navigate(`DetalhesPets`)}>
 
-              <View style={styles.cardInside}>
-                <Image source={require('../../../assets/iconPets.png')} style={styles.picimg} />
-                <Text style={styles.cardText}>Pets</Text>
-              </View>
+                <View style={styles.cardInside}>
+                  <Image source={require('../../../assets/iconPets.png')} style={styles.picimg} />
+                  <Text style={styles.cardText}>Pets</Text>
+                </View>
 
-            </TouchableOpacity>
+              </TouchableOpacity>
 
-            <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate(`DetalhesAlimentos`)}>
+              <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate(`DetalhesAlimentos`)}>
 
-              <View style={styles.cardInside}>
-                <Image source={require('../../../assets/iconFood.png')} style={styles.picimg} />
-                <Text style={styles.cardTextFood}>Alimentos/Água</Text>
-              </View>
+                <View style={styles.cardInside}>
+                  <Image source={require('../../../assets/iconFood.png')} style={styles.picimg} />
+                  <Text style={styles.cardTextFood}>Alimentos/Água</Text>
+                </View>
 
-            </TouchableOpacity>
+              </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.icones2}>
+              <TouchableOpacity style={styles.card} onPress={() => navigation.navigate(`DetalhesRoupas`)}>
+
+                <View style={styles.cardInside}>
+                  <Image source={require('../../../assets/iconCabide.png')} style={styles.picimg} />
+                  <Text style={styles.cardText}>Roupas</Text>
+                </View>
+
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate(`DetalhesHigiene`)}>
+
+                <View style={styles.cardInside}>
+                  <Image source={require('../../../assets/iconHigiene.png')} style={styles.picimg} />
+                  <Text style={styles.cardText}>Higiene</Text>
+                </View>
+
+              </TouchableOpacity>
+
+            </View>
+
+
+
+            <View style={styles.icones3}>
+              <TouchableOpacity style={styles.cardPix} onPress={() => navigation.navigate(`DetalhesPix`)}>
+
+                <View style={styles.cardInsidePix}>
+                  <Image source={require('../../../assets/iconPix.png')} style={styles.picimgPix} />
+                  <Text style={styles.cardTextPix}>Ajude pelo Pix</Text>
+                </View>
+
+              </TouchableOpacity>
+            </View>
 
           </View>
-
-          <View style={styles.icones2}>
-            <TouchableOpacity style={styles.card} onPress={() => navigation.navigate(`DetalhesRoupas`)}>
-
-              <View style={styles.cardInside}>
-                <Image source={require('../../../assets/iconCabide.png')} style={styles.picimg} />
-                <Text style={styles.cardText}>Roupas</Text>
-              </View>
-
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate(`DetalhesHigiene`)}>
-
-              <View style={styles.cardInside}>
-                <Image source={require('../../../assets/iconHigiene.png')} style={styles.picimg} />
-                <Text style={styles.cardText}>Higiene</Text>
-              </View>
-
-            </TouchableOpacity>
-
-          </View>
-
-         
-
-          <View style={styles.icones3}>
-            <TouchableOpacity style={styles.cardPix} onPress={() => navigation.navigate(`DetalhesPix`)}>
-
-              <View style={styles.cardInsidePix}>
-                <Image source={require('../../../assets/iconPix.png')} style={styles.picimgPix} />
-                <Text style={styles.cardTextPix}>Ajude pelo Pix</Text>
-              </View>
-
-            </TouchableOpacity>
-          </View>
-
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </ScrollView>
     </View>
-    
+
   );
 }
--- src/screens/Category/styles.js
+++ src/screens/Category/styles.js
@@ -3,13 +3,14 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
+    
   },
   imgBg: {
     flex: 1,
     alignItems: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
+    paddingBottom:"30%"
   },
   titulo: {
     fontSize: 60,
