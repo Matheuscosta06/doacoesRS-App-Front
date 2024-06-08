94bc335a0b9ceee4ff8fda1f2d2e73d229495171
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-06-08 19:54:33 -0300

    refactor: Update GiftDetails screen to use react-native components

--- src/screens/GiftDetails/index.jsx
+++ src/screens/GiftDetails/index.jsx
@@ -3,28 +3,36 @@ import styles from './styles';
 import { View, Text, TouchableOpacity, ScrollView, Image, TextInput } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import { useState, useEffect } from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function giftDetails({ route }) {
     const { product } = route.params
     const navigation = useNavigation();
 
-    const [qtd, setQtd] = useState(0);
+    const [qtd, setQtd] = useState('');
 
     return (
-        <View>
-            <Image source={`${product.image}`} style={styles.img} />
-            <Text>{product.type}</Text>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <TextInput
-                style={styles.input}
-                value={qtd}
-                onChangeText={setQtd}
-                placeholder='Insira a quantidade desejada doar'
-            />
+        <View style={styles.mainContainer}>
+            <View style={styles.imgContainer}>
+                <Image source={`${product.image}`} style={styles.img} />
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.txt}>{product.type}</Text>
+                <Text style={styles.txt}>{product.name}</Text>
+                <Text style={styles.txt}>{product.description}</Text>
+                <TextInput
+                    style={styles.input}
+                    value={qtd}
+                    onChangeText={setQtd}
+                    placeholder='Insira a quantidade desejada doar'
+                />
 
-            <TouchableOpacity onPress={() => navigation.navigate("Gift")}>
-                <Text>Voltar</Text>
+                <TouchableOpacity style={styles.addDonations}>
+                    <Text style={styles.txtDonations}>Adicionar a sua doação!!!</Text>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.arrowBack} onPress={() => navigation.navigate("Gift")}>
+                <AntDesign name="left" size={24} color="#FF6347" />
             </TouchableOpacity>
         </View>
     )
--- src/screens/GiftDetails/styles.js
+++ src/screens/GiftDetails/styles.js
@@ -1,15 +1,59 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: "#24396e",
+        paddingBottom: "100%",
+        width: '100%',
+        alignItems: 'center',
+    },
+    imgContainer: {
+        alignItems: 'center',
+        backgroundColor: '#d7d7d7',
+        padding: 20,
+        width: '100%',
+    },
     img: {
-        width: 194,
+        width: '100%',
         height: 194,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.45,
+        shadowRadius: 3.84,
+        elevation: 5,
+
+    },
+    infoContainer: {
+        backgroundColor: 'white',
+        padding: 20,
+        alignItems: 'center',
+        width: '90%',
+        height: '100%',
+        marginTop: 20,
+        borderRadius: 20,
+        borderColor: '#fda31b',
+        borderWidth: 5,
+    },
+    txt: {
+        color: "black",
+        fontSize: 20,
+        margin: 10,
+        fontFamily: "JosefinSans_700Bold",
     },
     input: {
         color: "lightgray",
         fontSize: 20,
         fontFamily: "JosefinSans_500Medium",
-    
-      },
+
+    },
+    arrowBack: {
+        position: 'absolute',
+        top: 15,
+        left: 15,
+    },
 });
 export default styles;
